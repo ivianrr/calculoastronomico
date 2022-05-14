ef6e3ff82fb94f793492c8fd2c3d0f090cd315a7
Ivan <ivianrr@gmail.com>
2022-05-14 18:20:50 +0200

    implemented a couple of functions from 0B

--- scripts/00-testrutinas.py
+++ scripts/00-testrutinas.py
@@ -4,3 +4,10 @@ from calculoastronomico.constant import *
 rad=cf.degree_to_radian(180)
 
 print("degree_to_radian",rad)
+
+print(cf.degree_to_ddmmss(-12.513))
+print(cf.degree_to_ddmmss(9.9999999999999931))
+
+print(cf.degree_to_ddmmss_compact(-12.513))
+
+print(cf.ddmmss_to_degree(*cf.degree_to_ddmmss(-12.513)))
--- src/calculoastronomico/formats.py
+++ src/calculoastronomico/formats.py
@@ -7,5 +7,35 @@ def degree_to_radian(deg):
     """Convierte la cantidad de grados a radianes"""
     return deg/180*PI
 
+def radian_to_degree(rad):
+    """Convierte la cantidad de radianes a grados"""
+    return rad/PI*180
+    
+# degree_to_ddmmss 
+def degree_to_ddmmss(degree):
+    """Convierte grados a formato grados, minutos, segundos"""
+    sig=math.copysign(1.0, degree)
+    ad=abs(degree)
+    d=math.floor(ad)
+    dm=(ad-d)*60
+    m=math.floor(dm)
+    s=(dm-m)*60
+    return [sig,d,m,s]
+
+# degree_to_ddmmss_compact 
+def degree_to_ddmmss_compact(degree):
+    """Convierte grados a formato compacto dd.mmss"""
+    sig,d,m,s=degree_to_ddmmss(degree)
+    return sig*(d+m*1e-2+s*1e-4)
+
+
+# ddmmss_to_degree 
+def ddmmss_to_degree(sig,d,m,s):
+    """Formato grados, minutos, segundos a grados"""
+    return sig*(d+(m+s/60)/60)
+
+
+# ddmmss_compact_to_degree 
+# scale_angle
 
 
