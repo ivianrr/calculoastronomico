18905ff442d8b27ab4b625bd7574ed74945e5a38
Ivan <ivianrr@gmail.com>
2022-05-15 02:10:27 +0200

    trying to fix  accuracy regarding compact ddmms

--- scripts/00-testrutinas.py
+++ scripts/00-testrutinas.py
@@ -11,3 +11,19 @@ print(cf.degree_to_ddmmss(9.9999999999999931))
 print(cf.degree_to_ddmmss_compact(-12.513))
 
 print(cf.ddmmss_to_degree(*cf.degree_to_ddmmss(-12.513)))
+
+
+x=-12.513
+print("degrees",x)
+print("ddmmss",cf.degree_to_ddmmss(x))
+print("ddmmsscompact",cf.degree_to_ddmmss_compact(x))
+print("back to degrees",cf.ddmmss_compact_to_degree(cf.degree_to_ddmmss_compact(x)))
+
+x=40.16
+print("accuracy check: ddmmss_c=",x)
+print("deg:",cf.ddmmss_compact_to_degree(x))
+print("back to compact:",cf.degree_to_ddmmss_compact(cf.ddmmss_compact_to_degree(x)))
+
+print("back to degree one last time:",cf.ddmmss_compact_to_degree(cf.degree_to_ddmmss_compact(cf.ddmmss_compact_to_degree(x))))
+print("back to compact one last time:",cf.degree_to_ddmmss_compact(cf.ddmmss_compact_to_degree(cf.degree_to_ddmmss_compact(cf.ddmmss_compact_to_degree(x)))))
+
--- src/calculoastronomico/formats.py
+++ src/calculoastronomico/formats.py
@@ -20,9 +20,13 @@ def degree_to_ddmmss(degree):
     dm=(ad-d)*60
     m=math.floor(dm)
     s=(dm-m)*60
+    # print([sig,d,m,s])
+    # print("prueba",sig*(d+(m+s/60)/60))
     return [sig,d,m,s]
 
 # degree_to_ddmmss_compact 
+# some problems here with accuracy
+# performing the operation on [1.0, 40, 15, 59.99999999997101] results in 40.156, which is equivalent to 40.16 if the 6 is carried over.
 def degree_to_ddmmss_compact(degree):
     """Convierte grados a formato compacto dd.mmss"""
     sig,d,m,s=degree_to_ddmmss(degree)
@@ -36,6 +40,23 @@ def ddmmss_to_degree(sig,d,m,s):
 
 
 # ddmmss_compact_to_degree 
+# TODO: check accuracy, floor. in degree_to_ddmmssit doesnt matter but in this case it does
+def ddmmss_compact_to_degree(x):
+    """Convierte grados, minutos y segundos en formato compacto a grados"""
+    sig=math.copysign(1.0, x)
+    da=abs(x)
+    d=math.floor(da)
+    dm=(da-d)*100
+    if dm>90:   # it should be enough to check for this
+        dm-=40
+    m=math.floor(dm)
+    s=(dm-m)*100
+    if s>90:   # it should be enough to check for this
+        s-=40
+    print(sig,d,m,s)
+    # print(sig*( d*3600+ m*60+ s)/3600)
+    return sig*(d+(m+s/60.)/60.)
+
 # scale_angle
 
 
