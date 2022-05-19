720b80318401b93417555a3ad6d2a38b2a12bf74
Ivan <ivianrr@gmail.com>
2022-05-19 19:32:00 +0200

    Last optional routines from 0B

--- src/calculoastronomico/formats.py
+++ src/calculoastronomico/formats.py
@@ -7,12 +7,12 @@ from calculoastronomico.constant import *
 
 def degree_to_radian(deg):
     """Convierte la cantidad de grados a radianes"""
-    return deg/180*PI
+    return deg / 180 * PI
 
 
 def radian_to_degree(rad):
     """Convierte la cantidad de radianes a grados"""
-    return rad/PI*180
+    return rad / PI * 180
 
 
 def degree_to_ddmmss(degree):
@@ -20,9 +20,9 @@ def degree_to_ddmmss(degree):
     sig = math.copysign(1.0, degree)
     ad = abs(degree)
     d = math.floor(ad)
-    dm = (ad-d)*60
+    dm = (ad - d) * 60
     m = math.floor(dm)
-    s = (dm-m)*60
+    s = (dm - m) * 60
     return [sig, d, m, s]
 
 
@@ -31,12 +31,12 @@ def degree_to_ddmmss_compact(degree):
     # some problems here with accuracy
     # performing the operation on [1.0, 40, 15, 59.99999999997101] results in 40.156, which is equivalent to 40.16 if the 6 is carried over.
     sig, d, m, s = degree_to_ddmmss(degree)
-    return sig*(d+(m+s/100)/100)
+    return sig * (d + (m + s / 100) / 100)
 
 
 def ddmmss_to_degree(sig, d, m, s):
     """Formato grados, minutos, segundos a grados"""
-    return sig*(d+(m+s/60)/60)
+    return sig * (d + (m + s / 60) / 60)
 
 
 def ddmmss_compact_to_degree(x):
@@ -44,67 +44,83 @@ def ddmmss_compact_to_degree(x):
     sig = math.copysign(1.0, x)
     da = abs(x)
     d = math.floor(da)
-    dm = (da-d)*100
+    dm = (da - d) * 100
     if dm > 90:
         dm -= 40
     m = math.floor(dm)
-    s = (dm-m)*100
+    s = (dm - m) * 100
     if s > 90:
         s -= 40
-    return sig*(d+(m+s/60.)/60.)
+    return sig * (d + (m + s / 60.0) / 60.0)
 
 
 def scale_angle(a):
     """Sitúa un ángulo en [0,2p)"""
-    return a-2*PI*math.floor(a/(2*PI))
+    return a - 2 * PI * math.floor(a / (2 * PI))
+
 
 #
 # Rutinas opcionales
 #
 
 
-# degree_to_ddmm: convierte grados a grados y minutos
-# degree_to_ddmm_compact: convierte grados a grados y minutos en formato compacto
-# ddmm_to_degree: convierte grados y minutos a grados
-# ddmm_compact_to_degree: misma que anterior pero en formato compacto
+# -degree_to_ddmm: convierte grados a grados y minutos
+# -degree_to_ddmm_compact: convierte grados a grados y minutos en formato compacto
+# -ddmm_to_degree: convierte grados y minutos a grados
+# -ddmm_compact_to_degree: misma que anterior pero en formato compacto
 
 # hour_to_radian: convierte horas a radianes
 def hour_to_radian(h):
     """convierte horas a radianes"""
-    return h/12*PI
+    return h / 12 * PI
+
 
 # radian_to_hour: convierte radianes a horas
 def radian_to_hour(rad):
-    """    convierte radianes a horas"""
-    return rad*12/PI
+    """convierte radianes a horas"""
+    return rad * 12 / PI
 
 
 # degree_to_hour: convierte grados a horas
-def degree_to_hour(deg): 
+def degree_to_hour(deg):
     """convierte grados a horas"""
-    return deg/15
+    return deg / 15
+
 
 # hour_to_degree: convierte horas a grados
-def hour_to_degree(h): 
+def hour_to_degree(h):
     """convierte horas a grados"""
-    return h*15
+    return h * 15
 
-# hour_to_hhmmss: convierte horas a horas), minutos y segundoss
+
+# hour_to_hhmmss: convierte horas a horas), minutos y segundos
+def hour_to_hhmmss(h):
+    """Convierte horas a horas, minutos y segundos"""
+    return degree_to_ddmmss(h)
+
+
 # -hour_to_hhmmss_compact: misma que anterior, pero en formato compacto
+
 # hhmmss_to_hour: convierte horas, minutos y segundos a horas
+def hhmmss_to_hour(sig, h, m, s):
+    """Convierte horas, minutos y segundos a horas"""
+    return ddmmss_to_degree(sig, h, m, s)
+
+
 # -hhmmss_compact_to_hour: misma que anterior, pero en formato compacto
-# hour_to_hhmm: convierte horas a horas y minutos
+# -hour_to_hhmm: convierte horas a horas y minutos
 # -hour_to_hhmm_compact: misma que anterior pero en formato compacto
-# hhmm_to_hour: convierte horas y minutos a horas
+# -hhmm_to_hour: convierte horas y minutos a horas
 # -hhmm_compact_to_hour: misma que anterior pero en formato compacto
 
 
 # scale_hour: sitúa una hora en formato decimal en el intervalo [0,24)
-def scale_hour(h): 
+def scale_hour(h):
     """sitúa una hora en formato decimal en el intervalo [0,24)"""
-    return h-24*math.floor(h/24)
+    return h - 24 * math.floor(h / 24)
+
 
 # scale_degree: sitúa un ángulo en grados en formato decimal en el intervalo [0,360)
-def scale_degree(deg): 
+def scale_degree(deg):
     """sitúa un ángulo en grados en formato decimal en el intervalo [0,360)"""
-    return deg-360*math.floor(deg/360)
+    return deg - 360 * math.floor(deg / 360)
